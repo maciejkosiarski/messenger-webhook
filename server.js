9ede8ce40da6287cb4ae9923bb6a3deed2cd721b
Maciej Kosiarski <maciek.kosiarski@gmail.com>
2019-01-24 23:48:40 +0100

    server.js modified

--- server.js
+++ server.js
@@ -17,17 +17,13 @@ app.get('/webhook', (req, res) => {
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
-
     // Checks if a token and mode is in the query string of the request
     if (mode && token) {
-
         // Checks the mode and token sent is correct
         if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
-
             // Responds with the challenge token from the request
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
-
         } else {
             // Responds with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);
@@ -37,20 +33,15 @@ app.get('/webhook', (req, res) => {
 
 app.post('/webhook', (req, res) => {
     let body = req.body;
-
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
-
         body.entry.forEach(function (entry) {
             // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
-
-
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
-
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
             if (webhook_event.message) {
@@ -59,30 +50,25 @@ app.post('/webhook', (req, res) => {
                 handlePostback(sender_psid, webhook_event.postback);
             }
         });
-
         // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
     } else {
         // Returns a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
     }
-
 });
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
     let response;
-
     // Check if the message contains text
     if (received_message.text) {
-
         // Create the payload for a basic text message
         response = {
-            "text": `Napisałeś: "${received_message.text}". I co ja mam z tym zrobić?`
+            "text": `Moduł konwersacji nie został jeszcze zaimplementowany. Nie mogę odpowiedzieć na: "${received_message.text}".`
             //"text": `You sent the message: "${received_message.text}". Now send me an image!`
         }
     }
-
     // Sends the response message
     callSendAPI(sender_psid, response);
 }
